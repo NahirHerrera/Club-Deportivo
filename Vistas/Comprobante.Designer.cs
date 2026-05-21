f485d282d4a9da5b2ceb1509d374d54e0319c684
Nahir Herrera <naherrera.7@gmail.com>
2026-05-21 18:43:16 -0300

    Formulario - Comprobante de pago

--- Vistas/Comprobante.Designer.cs
+++ Vistas/Comprobante.Designer.cs
@@ -31,16 +31,28 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Comprobante));
             textBox1 = new TextBox();
             pictureBox1 = new PictureBox();
-            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            lbl_Fecha = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(193, 22);
+            textBox1.BackColor = Color.Tan;
+            textBox1.Font = new Font("Lucida Fax", 12F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(261, 12);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(401, 31);
+            textBox1.Size = new Size(280, 31);
             textBox1.TabIndex = 0;
             textBox1.Text = "COMPROBANTE DE PAGO";
             textBox1.TextAlign = HorizontalAlignment.Center;
@@ -50,38 +62,145 @@
             pictureBox1.ErrorImage = (Image)resources.GetObject("pictureBox1.ErrorImage");
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
-            pictureBox1.Location = new Point(313, 73);
+            pictureBox1.Location = new Point(117, 110);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(145, 144);
+            pictureBox1.Size = new Size(92, 91);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
-            // tableLayoutPanel1
+            // panel1
             // 
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Location = new Point(208, 273);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 2;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(250, 125);
-            tableLayoutPanel1.TabIndex = 2;
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(38, 223);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(731, 193);
+            panel1.TabIndex = 2;
+            // 
+            // lbl_Fecha
+            // 
+            lbl_Fecha.AutoSize = true;
+            lbl_Fecha.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_Fecha.Location = new Point(432, 102);
+            lbl_Fecha.Name = "lbl_Fecha";
+            lbl_Fecha.Size = new Size(205, 23);
+            lbl_Fecha.TabIndex = 3;
+            lbl_Fecha.Text = "Fecha: 05/05/2026";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.WindowFrame;
+            label1.Location = new Point(75, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 23);
+            label1.TabIndex = 5;
+            label1.Text = "CLUB DEPORTIVO";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(88, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(154, 23);
+            label3.TabIndex = 6;
+            label3.Text = "Fitness Center";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(432, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(305, 23);
+            label2.TabIndex = 7;
+            label2.Text = "Dirección: Bulnes 456 - CABA";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(432, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(288, 23);
+            label4.TabIndex = 8;
+            label4.Text = "Teléfono: (011)  4563-7845";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Lucida Fax", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(432, 65);
+            label5.Name = "label5";
+            label5.Size = new Size(229, 23);
+            label5.TabIndex = 9;
+            label5.Text = "CUIT: 30-23654878-9";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Lucida Fax", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(38, 16);
+            label6.Name = "label6";
+            label6.Size = new Size(133, 20);
+            label6.TabIndex = 10;
+            label6.Text = "N° Socio: 1245";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Lucida Fax", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(38, 60);
+            label7.Name = "label7";
+            label7.Size = new Size(319, 20);
+            label7.TabIndex = 11;
+            label7.Text = "Nombre y apellido: Carolina Gomez";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Lucida Fax", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(37, 103);
+            label8.Name = "label8";
+            label8.Size = new Size(300, 20);
+            label8.TabIndex = 12;
+            label8.Text = "Concepto/Actividad: Musculación";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Lucida Fax", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(38, 151);
+            label9.Name = "label9";
+            label9.Size = new Size(187, 20);
+            label9.TabIndex = 13;
+            label9.Text = "Período: MAYO 2026";
             // 
             // Comprobante
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.DarkKhaki;
+            BackColor = Color.Khaki;
             ClientSize = new Size(800, 450);
-            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(lbl_Fecha);
+            Controls.Add(panel1);
             Controls.Add(pictureBox1);
             Controls.Add(textBox1);
             Name = "Comprobante";
             Text = "Comprobante de pago";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -90,6 +209,16 @@
 
         private TextBox textBox1;
         private PictureBox pictureBox1;
-        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Label lbl_Fecha;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label9;
     }
 }
