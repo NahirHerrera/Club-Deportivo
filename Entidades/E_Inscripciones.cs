aa51049c64cb1d86560095815769b41247b7618b
Gaston Enriotti <gastonenriotti@Gastons-MacBook-Air.local>
2026-05-24 06:26:39 -0300

    POO: Persona Socio NoSocio + cuotas

---
+++ Entidades/E_Inscripciones.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace Club_Deportivo.Entidades
+{
+    internal class E_Inscripcion
+    {
+        public int IdInscripcion { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int IdSocio { get; set; }
+
+        public int IdActividad { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
---
+++ Entidades/E_NoSocio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Club_Deportivo.Entidades
+{
+    internal class E_NoSocio : E_Persona
+    {
+        public DateTime FechaVisita { get; set; }
+        public decimal CostoActividad { get; set; }
+
+        // Asociación
+        public E_Actividad Actividad { get; set; }
+
+        public override string ObtenerTipoPersona()
+        {
+            return "No Socio";
+        }
+    }
+}
