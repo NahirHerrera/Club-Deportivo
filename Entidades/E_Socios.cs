9c95d05e9d9fcce3299b1e9f0f36c27e302c91f4
Nahir Herrera <naherrera.7@gmail.com>
2026-05-29 08:40:35 -0300

    Corrijo registros de socios y no socios

--- Entidades/E_Socios.cs
+++ Entidades/E_Socios.cs
@@ -8,5 +8,17 @@ namespace Club_Deportivo.Entidades
     {
         public int nroCarnet {  get; set; }
         public DateTime fecha_vencimiento_cuota {  get; set; } 
+        public double montoCuota { get; set; }
+
+        public E_Socios(int id, string nombre, string apellido, string tipoDoc, string dni, bool aptoFisico) : base(id, nombre, apellido, tipoDoc, dni, aptoFisico) { }
+        public override string generarInscripcion()
+        {
+            return "$SOCIO: {nombre} {apellido} - Carnet: {nroCarnet}";
+        }
+        public override double pagarCuota()
+        {
+            return montoCuota;
+        }
+        public void obtenerCarnet() { }
     }
 }
