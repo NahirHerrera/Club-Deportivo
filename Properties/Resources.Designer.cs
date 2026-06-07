44f86f7046872e210fc027d5c3cb4eb3a46fe51a
Nahir Herrera <naherrera.7@gmail.com>
2026-06-07 18:38:55 -0300

    Final

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Club_Deportivo.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap exito {
-            get {
-                object obj = ResourceManager.GetObject("exito", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap login {
             get {
                 object obj = ResourceManager.GetObject("login", resourceCulture);
